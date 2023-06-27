7c805f37522cfadd448a0d3e44bbaf2b7c80410d
egorb <egorbusalaev@gmail.com>
2023-06-27 19:54:50 +0700

    добавил в пом зависимости  c3p0 и postgresql, hibernate но добавил т.к. сам запросы писать не буду
    @Autowired
      DataSource dataSource; в сукьюрити конфиг dependency ing
    
     в БД создали юзеров, в конфиг секьюрити не нужны
    
        auth.jdbcAuthentication().dataSource(dataSource); ->указал где брать пароли в БД

--- src/main/java/com/egor/spring/security/configuration/MyConfig.java
+++ src/main/java/com/egor/spring/security/configuration/MyConfig.java
@@ -1,5 +1,6 @@
 package com.egor.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,17 +8,34 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
 @Configuration
 @ComponentScan(basePackages = "com.egor.spring.security")
 @EnableWebMvc
 public class MyConfig {
 
     @Bean
-    public ViewResolver viewResolver(){
+    public ViewResolver viewResolver() {
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
         internalResourceViewResolver.setPrefix("/WEB-INF/view/");
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("org.postgresql.Driver");
+            dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/company_repository?useSSL=false&amp;serverTimezone=UTC");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("java");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
     }
 }
 
--- src/main/java/com/egor/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/egor/spring/security/configuration/MySecurityConfig.java
@@ -1,26 +1,32 @@
 package com.egor.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication().withUser(userBuilder.username("egor").password("egor").roles("EMPLOYEE"))
-        .withUser(userBuilder.username("vika").password("vika").roles("Manager"))
-        .withUser(userBuilder.username("fedor").password("fedor").roles("HR", "Manager"));
+        auth.jdbcAuthentication().dataSource(dataSource);
+//        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();                 // в БД создали этих юзеров и пароли, поэтому тут не нужны
+//        auth.inMemoryAuthentication().withUser(userBuilder.username("egor").password("egor").roles("EMPLOYEE"))
+//        .withUser(userBuilder.username("vika").password("vika").roles("Manager"))
+//        .withUser(userBuilder.username("fedor").password("fedor").roles("HR", "Manager"));
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.authorizeRequests().antMatchers("/").hasAnyRole("Manager","HR", "EMPLOYEE")
+       http.authorizeRequests().antMatchers("/").hasAnyRole("MANAGER","HR", "EMPLOYEE")
                .antMatchers("/hr_info").hasRole("HR")
-               .antMatchers("/manager_info/**").hasRole("Manager").// звездочки- менеджеру достумны все view с началом /manager_info
+               .antMatchers("/manager_info/**").hasRole("MANAGER").// звездочки- менеджеру достумны все view с началом /manager_info
                and().formLogin().permitAll();
     }
 }
